4745a6cb0d609b8efc3eaa44358fd895bc8f23d8
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-07-29 10:32:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var main = function() {
   $("body").click(function() {
-    $(".slideshowimage").fadeOut(1000);
-    $(".slideshowimage").fadeIn(1000);
+    $("#slideshowimage").fadeOut(1000);
+    $("#slideshowimage").fadeIn(1000);
   });
 
-  $(".slideshowbutton-right").click(function() {
-    $(".slideshowimage").fadeOut(1000);
-    $(".slideshowimage").fadeIn(1000);
+  $("#slideshowbutton-right").click(function() {
+    $("#slideshowimage").fadeOut(1000);
+    $("#slideshowimage").fadeIn(1000);
   });
 }
 
