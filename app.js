9055d40b1a1fb6820a7f722340a157db60490fd1
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-07-29 11:50:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var main = function() {
   $("#slideshowbutton-next").click(function() {
     var currentSlide = $(".activeslide");
     var nextSlide = currentSlide.next();
+    var currentCaption = $(".activecaption");
+    var nextCaption = currentCaption.next();
     if (nextSlide.length === 0) {
       nextSlide = $(".slideshow").first();
-      nextSlide.addClass('new');
+      nextCaption = $(".slideshow-captions").first();
     };
   });
 }
