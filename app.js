1a31b4ebeed9be6a837cdce7787477c17cedd6c5
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-07-29 11:15:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var main = function() {
     var currentSlide = $(".activeslide");
     var nextSlide = currentSlide.next();
     if (nextSlide.is('h1')) {
+    }
+    else {
       $('body').addClass('red');
     };
   });
