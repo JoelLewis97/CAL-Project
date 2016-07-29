f4c6a0ed0c9e2ae65be21510bfb78a4f2f809a9d
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-07-29 12:52:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var main = function() {
     currentCaption.fadeOut(1000).removeClass("activecaption");
     nextSlide.fadeIn(1000).addClass("activeslide");
     nextCaption.fadeIn(1000).addClass("activecaption");
-    window.alert(currentSlide.prop('tagName') + "kthen");
+    window.alert(nextSlide.prop('tagName'));
   });
 }
 
