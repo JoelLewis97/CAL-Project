4ad17df89232a0f4bc8d32f6de73ac9198609db0
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-07-29 10:32:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var main = function() {
-  $("body").click(function() {
-    $("#slideshowimage").fadeOut(1000);
-    $("#slideshowimage").fadeIn(1000);
-  });
-
-  $("#slideshowbutton-right").click(function() {
+  $("#slideshowbutton").click(function() {
     $("#slideshowimage").fadeOut(1000);
     $("#slideshowimage").fadeIn(1000);
   });
