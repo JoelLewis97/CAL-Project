f13f46b344b3a1f76d19bd0c9887ca1e12fc40b0
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-07-29 11:01:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ var main = function() {
   $("#slideshowbutton").click(function() {
     $(".activeslide").fadeOut(1000).removeClass('activeslide');
     $(".activecaption").fadeOut(1000).removeClass('.activecaption');
-    $(".nextslide").fadeIn(1000).addClass("activeslide");
-    $(".nextslide").removeClass("nextslide");
-    $(".nextcaption").fadeIn(1000).addClass("activecaption");
-    $(".nextcaption").removeClass("nextcaption");
+    $(".activeslide").next().fadeIn(1000).addClass("activeslide");
+    $(".activecaption").next()fadeIn(1000).addClass("activecaption");
   });
 }
 
