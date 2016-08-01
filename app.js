048f1a5e3b72f94a6007eefd2df708ad84b797a5
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-08-01 18:41:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var main = function() {
       nextSlide = $("ul.slideshow li:first img")
       nextCaption = $("ul.slideshow-captions li:first")
     };
-    window.alert(currentSlide.get(0).tagName + "123" + nextSlide.get(0).tagName + "345" + currentCaption.get(0).tagName + "678" + nextCaption.get(0).tagName)
+    window.alert(currentSlide.length + "dd" + nextSlide.length)
     currentSlide.fadeOut(1000).removeClass("activeslide");
     currentCaption.fadeOut(1000).removeClass("activecaption");
     nextSlide.fadeIn(1000).addClass("activeslide");
