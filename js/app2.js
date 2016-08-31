0f6ca1f0cfa2b8eb422b19e1f4cb876986c96b96
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-08-31 15:19:30 +0100

    Update app2.js

--- js/app2.js
+++ js/app2.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('.slickslide').slick({
     autoplay: true,
     autoplaySpeed: 5000,
-    prevArrow: "<img src='https://cdn1.iconfinder.com/data/icons/general-9/500/left_arrow-128.png' style='height: 50px; width 50px; display: inline-block;'/>",
-    nextArrow: "<img src='https://cdn1.iconfinder.com/data/icons/general-9/500/right_arrow-128.png' style='height: 50px; width 50px; display: inline-block;'/>"
+    prevArrow: "<img src='https://cdn1.iconfinder.com/data/icons/general-9/500/left_arrow-128.png' style='height: 50px; width 50px;'/>",
+    nextArrow: "<img src='https://cdn1.iconfinder.com/data/icons/general-9/500/right_arrow-128.png' style='height: 50px; width 50px;'/>",
+    appendArrows: '.slickslide'
   });
 });
