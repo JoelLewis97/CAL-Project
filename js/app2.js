f98afe92ec9a72de1bb75649ba4aec033411997f
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-08-31 15:58:36 +0100

    Update app2.js

--- js/app2.js
+++ js/app2.js
@@ -2,7 +2,5 @@ $(document).ready(function(){
   $('.slickslide').slick({
     autoplay: true,
     autoplaySpeed: 5000,
-    prevArrow: "<img src='https://cdn1.iconfinder.com/data/icons/general-9/500/left_arrow-128.png' style='height: 50px; width 50px;'/>",
-    nextArrow: "<img src='https://cdn1.iconfinder.com/data/icons/general-9/500/right_arrow-128.png' style='height: 50px; width 50px;'/>",
   });
 });
