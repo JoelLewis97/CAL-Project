8c7c09b62ba4a87bcc2de6074696bc4f7f9ce503
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-09-20 12:43:22 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
   $('.jumboslide').slick({
+    infinite: true,
     touchMove: true,
     autoplay: true,
     autoplaySpeed: 5000
   });
   
-  $('.offerslide').slick();
+  $('.offerslide').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
 });
