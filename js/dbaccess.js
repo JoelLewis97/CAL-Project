9c8d56aa912f5de19d10742ad21d243982ab4d3d
Joel Lewis <JoelLewis97@users.noreply.github.com>
2016-10-18 14:58:08 +0100

    Create dbaccess.js

---
+++ js/dbaccess.js
@@ -0,0 +1,10 @@
+	var config = {
+		apiKey: "AIzaSyDbSBmmk6E6y-LrSH5WGG2mmNKuWy_RfqU",
+		authDomain: "cal-project-19c94.firebaseapp.com",
+		databaseURL: "https://cal-project-19c94.firebaseio.com",
+		storageBucket: "",
+		messagingSenderId: "78007112615"
+	};
+	firebase.initializeApp(config);
+	//Get a reference to the database service
+	var database = firebase.database();
